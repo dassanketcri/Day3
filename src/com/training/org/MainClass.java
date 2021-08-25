7af6466f926ebacb4a60a61517ab64bebfed6574
dassanketcri <sanketvd@maveric-systems.com>
2021-08-25 09:42:07 +0530

    Initial commit

---
+++ src/com/training/org/MainClass.java
@@ -0,0 +1,11 @@
+package com.training.org;
+
+public class MainClass {
+    public static void main(String[] args) {
+        XYZBank x1 = new XYZBank();
+        System.out.println(x1);
+
+        XYZBank x2 = new XYZBank("MyBank", "USA", 4000);
+        System.out.println(x2);
+    }
+}
